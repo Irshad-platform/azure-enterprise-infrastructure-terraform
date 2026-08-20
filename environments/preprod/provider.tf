f2d2161f54a2868f38c7b2e33e1e4e195cffdd2c
Irshad50 <mdirshadalam91190@gmail.com>
2026-08-20 08:11:39 +0530

    Backend update

--- environments/preprod/provider.tf
+++ environments/preprod/provider.tf
@@ -6,6 +6,12 @@ terraform {
       version = "4.80.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "Backend_Rg"
+    storage_account_name = "eric5storage09"
+    container_name       = "devesource"
+    key                  = "preprod_terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
